c0d9fa362b23eee279cecd47f14587fe42a8b2b4
Usama Mian <usamamian1995@gmail.com>
2019-09-17 17:58:14 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+export const sum = (x, y) => x + y;
 
 function App() {
   return (
     <div className="App">
+      <h2>Welcome to React</h2>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { configure } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
+configure({ adapter: new Adapter() });
+import { shallow } from "enzyme";
+import App from "./App";
+// import { sum } from "./App";
+const sum = jest.fn((x, y) => x + y);
+it("renders without crashing", () => {
+  const div = document.createElement("div");
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+test("Fake Test", () => {
+  console.log("usama");
+});
+
+it("sums numbers", () => {
+  expect(sum(1, 2)).toEqual(3);
+  expect(sum(2, 2)).toEqual(4);
+});
+it("renders welcome message", () => {
+  const wrapper = shallow(<App />);
+  const welcome = <h2>Welcome to React</h2>;
+  // expect(wrapper.contains(welcome)).toBe(true);
+  expect(wrapper.contains(welcome)).toEqual(true);
 });
